edec2fbf9790ba96786335f3bada2a4a7c72259e
aritropaul <aritro.paul61@gmail.com>
2019-04-08 10:15:19 +0530

    Search

---
+++ ElectionEye/View/SearchTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DistrictTableViewCell.swift
+//  ElectionEye
+//
+//  Created by Aritro Paul on 07/04/19.
+//  Copyright © 2019 Pranav Karnani. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.backgroundColor = .clear
+        resultLabel.textColor = .white
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ElectionEye/ViewControllers/SearchViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SearchViewController.swift
+//  
+//
+//  Created by Aritro Paul on 08/04/19.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    var array = [Any]()
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredLocations = [String]()
+    
+    @IBOutlet weak var searchTable: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(blurEffectView)
+        view.sendSubviewToBack(blurEffectView)
+        searchTable.backgroundColor = .clear
+        searchTable.delegate = self
+        searchTable.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        self.searchTable.reloadData()
+    }
+
+}
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell") as! SearchTableViewCell
+        cell.resultLabel.text = (array[indexPath.row] as! Constituency).name
+        return cell
+    }
+    
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+//        filterContentForSearchText(searchController.searchBar.text!)
+        // search logic here
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+}
